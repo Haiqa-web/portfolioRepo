06674ff39e055e613a42853244e7811741785fb1
Haiqa-web <haiqakhan3344@gmail.com>
2025-08-29 15:36:06 +0530

    fixed paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './Components/Navbar/NavBar.jsx'
+import Navbar from './Components/Navbar/Navbar.jsx'
 import Hero from './Components/Hero/Hero.jsx'
 import About from './Components/About/About.jsx'
 import Services from './Components/Services/Services.jsx'
