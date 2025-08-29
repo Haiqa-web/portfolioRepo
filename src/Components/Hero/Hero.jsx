8ad047e26cb2ad9620f0677d329f58af40304813
Haiqa-web <haiqakhan3344@gmail.com>
2025-08-29 15:58:16 +0530

    fixed

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Hero.css'
-import profile_img1 from '../../assets/profile_img1.jpg'
+import profile_img from '../../assets/profile_img1.jpg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
   return (
     <div id='home' className='hero'>
        <div className="img-container">
-         <img src={profile_img1} alt="" />
+         <img src={profile_img} alt="" />
        </div>
         <h1><span>I'm Haiqa Khan, </span>frontend developer based in India.</h1>
         <p>I am a passionate web developer from Jharkhand, India. I build modern, responsive web applications using React.js and Vite, focusing on clean code, smooth user experiences, and interactive designs. </p>
